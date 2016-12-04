3fd2ccca0ee563e11dca8334dd17c8290763ac0b
lakshmipreethi07 <tk_preethi@yahoo.com>
2016-12-04 23:17:26 +0530

    frontend

---
+++ frontend/collaborationfrontend/WebContent/usercontroller.js
@@ -0,0 +1,15 @@
+'use strict'
+app.controller('HomeController', function($scope) {
+	  $scope.message = 'Hello from HomeController';
+	});
+
+	app.controller('BlogController', function($scope) {
+	  $scope.message = 'Hello from BlogController';
+	});
+
+	app.controller('AboutController', function($scope) {
+	  $scope.message = 'Hello from AboutController';
+	});
+/**
+ * 
+ */
